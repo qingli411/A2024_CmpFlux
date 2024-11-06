2a5b79279c5e6fe1c9f5dd82e77f67f9ecdf1758
Qing Li <lqingpku@gmail.com>
2024-11-06 09:18:24 +0800

    Merge plots

--- notebooks/sbl_bbl.py
+++ notebooks/sbl_bbl.py
@@ -36,19 +36,53 @@ def inertial_period(lat=45):
     f = 2*Omega*np.sin(np.deg2rad(lat))
     return 2*np.pi/f
 
-def get_tslice(N2, T_io, T_bm=1.5*86400, T_am=3.5*86400):
-    z0, z1 = get_edges(N2)
+def nondim_da(da, H, Tf):
+    zdim = da.dims[0]
+    tdim = da.dims[1]
+    z_norm = da.coords[zdim].data/H
+    time_dtime = pd.to_datetime(da.coords[tdim].data)
+    time_sec = (time_dtime-time_dtime[0]).total_seconds()
+    time_norm = time_sec/Tf
+    da_new = da.assign_coords({zdim: z_norm, tdim: time_norm})
+    da_new.coords[zdim].attrs['long_name'] = '$z/H$'
+    da_new.coords[zdim].attrs['units'] = ''
+    da_new.coords[tdim].attrs['long_name'] = '$t/T_f$'
+    da_new.coords[tdim].attrs['units'] = ''
+    return da_new
+
+def nondim_das(das, **kwargs):
+    das_new = {}
+    for var in das.keys():
+        das_new[var] = nondim_da(das[var], **kwargs)
+    return das_new
+
+def get_merge(NN, Tf=inertial_period(lat=45), nTf0=2, nTf1=4):
+    z0, z1 = get_edges(NN)
     time_merge = z0.dropna(dim='time').time[-1]
-    print('Time of merge:')
-    print(pd.to_datetime(time_merge.data))
-    tstart1, tend1 = time_merge - pd.Timedelta(T_io, 's') - pd.Timedelta(T_bm, 's'), time_merge - pd.Timedelta(T_bm, 's')
-    tstart2, tend2 = time_merge + pd.Timedelta(T_am, 's'), time_merge + pd.Timedelta(T_am, 's') + pd.Timedelta(T_io, 's')
-    print('T1:')
-    print(pd.to_datetime(tstart1.data))
-    print(pd.to_datetime(tend1.data))
-    print('T2:')
-    print(pd.to_datetime(tstart2.data))
-    print(pd.to_datetime(tend2.data))
+    if time_merge.attrs['long_name'] == '$t/T_f$':
+        tstart1, tend1 = time_merge-1-nTf0, time_merge-nTf0
+        tstart2, tend2 = time_merge+nTf1, time_merge+nTf1+1
+        print('Time of merge:')
+        print('t/Tf = {:g}'.format(time_merge.data))
+        print('T1:')
+        print('t/Tf = {:g} -- {:g}'.format(tstart1.data, tend1.data))
+        print('T2:')
+        print('t/Tf = {:g} -- {:g}'.format(tstart2.data, tend2.data))
+    else:
+        tstart1, tend1 = time_merge - pd.Timedelta(Tf, 's') - pd.Timedelta(nTf0*Tf, 's'), time_merge - pd.Timedelta(nTf0*Tf, 's')
+        tstart2, tend2 = time_merge + pd.Timedelta(nTf1*Tf, 's'), time_merge + pd.Timedelta(nTf1*Tf, 's') + pd.Timedelta(Tf, 's')
+        print('Time of merge:')
+        print(pd.to_datetime(time_merge.data))
+        print('T1:')
+        print(pd.to_datetime(tstart1.data))
+        print(pd.to_datetime(tend1.data))
+        print('T2:')
+        print(pd.to_datetime(tstart2.data))
+        print(pd.to_datetime(tend2.data))
+    return z0, z1, tstart1.data, tend1.data, tstart2.data, tend2.data
+
+def get_tslice(NN, Tf, **kwargs):
+    _, _, tstart1, tend1, tstart2, tend2 = get_merge(NN, Tf=Tf, **kwargs)
     tslice1 = slice(tstart1, tend1)
     tslice2 = slice(tstart2, tend2)
     return tslice1, tslice2
